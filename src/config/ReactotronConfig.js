402fdc28b9ee966d41bfe8d4c825ccaa18c26391
Humberto Barbosa <humberto.obarbosa@gmail.com>
2020-03-27 22:56:07 -0300

    estilo form

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,9 @@
+import Reactotron from 'reactotron-react-native';
+// 10.0.3.2 metrobundler
+// '192.168.1.4'
+if (__DEV__) {
+  const tron = Reactotron.configure({ host: '10.0.3.2' })
+    .useReactNative()
+    .connect();
+  console.tron = tron;
+}
